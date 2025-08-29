9585d4e630faa31940fda4ce01f79a28f1ca18e5
we3km <whdbstlr1662@naver.com>
2025-08-29 14:44:58 +0900

    첫 프로젝트 생성

---
+++ mbtix/src/main/java/com/kh/mbtix/MbtixApplication.java
@@ -0,0 +1,13 @@
+package com.kh.mbtix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtixApplication.class, args);
+	}
+
+}
---
+++ mbtix/src/test/java/com/kh/mbtix/MbtixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.mbtix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
