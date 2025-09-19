f19fbe34e60d42ac1a161fc43d752469e79bec18
DOOUNUT <kisuk446@naver.com>
2025-09-19 17:50:48 +0900

    9/19 집가기전

--- mbtix/src/main/java/com/kh/mbtix/mypage/controller/MyPageController.java
+++ mbtix/src/main/java/com/kh/mbtix/mypage/controller/MyPageController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
